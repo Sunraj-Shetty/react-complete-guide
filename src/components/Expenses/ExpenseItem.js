cc9555ed1a5d3c8ac9f564bc6e1041f2907336b7
Sunraj-Shetty <sunraj.martha@raasoft.com>
2022-11-21 12:13:11 +0530

    Basics

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('Clicked!!!');
+  };
+  // document.getElementById('root').addEventListener()
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler/*() => {console.log('Clicked!')}*/}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
